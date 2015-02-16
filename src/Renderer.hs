2ce7e9033944752ff3c58b99e080b60b53910c43
Thomas Athorne <thomas.athorne@gmail.com>
2015-02-16 00:28:30 +0000

    Prettier picture.

--- src/Renderer.hs
+++ src/Renderer.hs
@@ -12,6 +12,7 @@ import Control.Applicative
 black = PixelRGBA8 0 0 0 255
 blue = PixelRGBA8 0 0 255 80
 green = PixelRGBA8 0 255 0 80
+red = PixelRGBA8 255 0 0 80
 
 renderJourney :: (Float,Float) -> Float -> V.Vector (Int,Int) -> Drawing px ()
 renderJourney (x,y) scale v =
@@ -23,16 +24,21 @@ renderJourney (x,y) scale v =
 
 main = do
   journeys <- mapM (getJourney "3000") [1..200]
-  journeys' <- mapM (getJourney "2000") [1..200] 
+  journeys' <- mapM (getJourney "3") [1..200]
+  journeys'' <- mapM (getJourney "2000") [1..200] 
   let scales = map (\j -> max (V.maximum (V.map fst j)) (V.maximum (V.map snd j))) journeys
-      scales' = map (\j -> max (V.maximum (V.map fst j)) (V.maximum (V.map snd j))) journeys' 
+      scales' = map (\j -> max (V.maximum (V.map fst j)) (V.maximum (V.map snd j))) journeys'
+      scales'' = map (\j -> max (V.maximum (V.map fst j)) (V.maximum (V.map snd j))) journeys''
       scale = fromIntegral (maximum scales) / 450
       scale' = fromIntegral (maximum scales) / 450
-      scl = max scale scale'
+      scale'' = fromIntegral (maximum scales) / 450  
+      scl = scale `max` scale' `max` scale''
       img = renderDrawing 1000 1000 black $ do
-              withTexture (uniformTexture blue) $
+              withTexture (uniformTexture red) $
                 mapM_ (renderJourney (500,500) scale) journeys
               withTexture (uniformTexture green) $
                 mapM_ (renderJourney (500,500) scale) journeys'
-  
+              withTexture (uniformTexture blue) $
+                mapM_ (renderJourney (500,500) scale) journeys''
+
   writePng "/home/thomas/Projects/personal/drivers/resources/journeys.png" img
