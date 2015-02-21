90fca8e86ee5699939ef55a53dd67b3125911cc7
Thomas Athorne <thomas.athorne@gmail.com>
2015-02-21 00:40:54 +0000

    Should be using Doubles.

--- src/Math.hs
+++ src/Math.hs
@@ -3,21 +3,26 @@ module Math where
 
 import qualified Data.Vector.Unboxed as V
 
-journeyTime :: V.Vector (Int,Int) -> Int
-journeyTime = V.length
 
-journeyDist :: V.Vector (Int,Int) -> Double
+
+-----------------
+
+journeyTime :: V.Vector (Double,Double) -> Double
+journeyTime = fromIntegral . V.length
+
+journeyDist :: V.Vector (Double,Double) -> Double
 journeyDist = mag . V.last
 
-differentiate :: V.Vector (Int,Int) -> V.Vector (Int,Int)
+differentiate :: V.Vector (Double,Double) -> V.Vector (Double,Double)
 differentiate v =
   let v' = V.drop 1 v
   in V.zipWith disp v v'
 
 ---------------
 
-mag :: (Int, Int) -> Double
-mag (x,y) = sqrt $ (fromIntegral x)^2 + (fromIntegral y)^2
+mag :: (Double, Double) -> Double
+mag (x,y) = sqrt $ x^2 + y^2
 
-disp :: (Int,Int) -> (Int,Int) -> (Int,Int)
+disp :: (Double, Double) -> (Double, Double) -> (Double, Double)
 disp (x,y) (z,w) = (z-x, w-y)
+
