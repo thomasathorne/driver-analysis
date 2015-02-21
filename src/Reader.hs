01a95f12ae5989415b609fb93e20e08fb2de8eb7
Thomas Athorne <thomas.athorne@gmail.com>
2015-02-21 00:41:20 +0000

    More Doubles.

--- src/Reader.hs
+++ src/Reader.hs
@@ -8,10 +8,10 @@ import qualified Data.Vector.Unboxed as V
 dataPath = "/home/thomas/Projects/personal/drivers/resources/drivers/"
 journeyPath d n = dataPath ++ d ++ "/" ++ show n ++ ".csv"
 
-getJourney :: String -> Int -> IO (V.Vector (Int, Int))
+getJourney :: String -> Int -> IO (V.Vector (Double,Double))
 getJourney d n = readJourney $ journeyPath d n
 
-readJourney :: FilePath -> IO (V.Vector (Int, Int))
+readJourney :: FilePath -> IO (V.Vector (Double, Double))
 readJourney path = do
   s' <- L.readFile path
   let (_ ,s) = readLine s'
@@ -22,12 +22,13 @@ parse = V.unfoldr step
           (Just (a,b), rest) -> Just ((a,b),rest)
           (Nothing, _) -> Nothing
 
-readLine :: L.ByteString -> (Maybe (Int, Int), L.ByteString)
+readLine :: L.ByteString -> (Maybe (Double, Double), L.ByteString)
 readLine s =
   let (line, rest) = L.break (=='\n') s
       mab = do (aTens, dotA) <- L.readInt line
                (aOnes, comma) <- L.readInt $ L.drop 1 dotA
                (bTens, dotB) <- L.readInt $ L.drop 1 comma
                (bOnes, _) <- L.readInt $ L.drop 1 dotB
-               return (aTens*10 + aOnes, bTens*10 + bOnes)
+               return (fromIntegral aTens + 0.1*fromIntegral aOnes,
+                       fromIntegral bTens + 0.1*fromIntegral bOnes)
   in (mab, L.drop 1 rest)
