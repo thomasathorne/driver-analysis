3c01db84b7c9ef819d7d7d6a805b368bd79b1b09
Thomas Athorne <thomas.athorne@gmail.com>
2015-02-21 00:41:04 +0000

    Geometric transformations.

--- src/Math.hs
+++ src/Math.hs
@@ -26,3 +26,16 @@ mag (x,y) = sqrt $ x^2 + y^2
 disp :: (Double, Double) -> (Double, Double) -> (Double, Double)
 disp (x,y) (z,w) = (z-x, w-y)
 
+rotate :: (Double,Double) -> (Double, Double) -> (Double, Double)
+rotate (rx,ry) (x,y) = (rx*x - ry*y, ry*x + rx*y)
+
+translate :: (Double,Double) -> (Double, Double) -> (Double, Double)
+translate (tx,ty) (x,y) = (tx+x,ty+y)
+
+inverse :: (Double,Double) -> (Double, Double)
+inverse p@(x,y) = let d2 = x^2 + y^2
+                  in (x/d2, -y/d2)
+
+upperHalf :: (Double,Double) -> (Double, Double)
+upperHalf (x,y) = (x,abs y) 
+                     
